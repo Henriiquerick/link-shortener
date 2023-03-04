e18e9e1c3add4170f9b8ffb3eac60f81233ab42e
Rickson Henrique (Xucacodev) <88748126+Henriiquerick@users.noreply.github.com>
2023-03-04 20:35:14 -0300

    final adjusts on script

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
-
+const BTN2 = document.querySelector('#btn2')
+const inputValue = document.querySelector('#input-url')
 function shortener(){
-    const link = document.getElementById("input-url").value;
-    if(!link){
+    let url = document.getElementById("input-url").value;
+    if(!url){
         return alert('Needs URL for use URL Shortener');
     }
+
+    //headers
+    let headers = {
+        "Content-Type": "application/json",
+        "apiKey": "c0d55bc990644687a24acbbbca1da741"
+    };
+
+    let linkRequest = {
+        destination: url,
+        domain: { fullName: "rebrand.ly" }
+    }
+
+    fetch("https://api.rebrandly.com/v1/links", {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(linkRequest)
+    })
+    .then(response => response.json())
+    .then(json => {
+        console.log(json);
+        let inputUrl = document.getElementById("input-url");
+        inputUrl.value = json.shortUrl;
+    });
 }
+
+BTN2.addEventListener('click', async() => {
+    await navigator.clipboard.writeText(inputValue.value);
+    !alert('Copied link!')
+})
